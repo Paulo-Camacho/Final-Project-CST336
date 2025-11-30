bcf199cbae686632f1bae544d82dea30de738d9c
Paulo-Camacho <anthropologyparliament@gmail.com>
2025-11-29 17:56:16 -0800

    Adding login logic

--- index.mjs
+++ index.mjs
@@ -1,57 +1,73 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
-import session from "express-session";
+import session from 'express-session';
 import bcrypt from 'bcrypt';
 
-
 const app = express();
+
+/////////////// This is middleware for express-session
+app.use(
+  session({
+    secret: "superSecretKey123",    // any long random string
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Make session available in all EJS templates
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+//////////////////////////////////////////////////
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //for Express to get values using POST method
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //setting up database connection pool
 const pool = mysql.createPool({
-    host: "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "toszv1mikaqtn04s",
-    password: "m35rbj01t9klhgh8",
-    database: "g4ekc2ffehhxsjqq",
-    connectionLimit: 10,
-    waitForConnections: true
+  host: 'k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'toszv1mikaqtn04s',
+  password: 'm35rbj01t9klhgh8',
+  database: 'g4ekc2ffehhxsjqq',
+  connectionLimit: 10,
+  waitForConnections: true,
 });
 //routes
 app.get('/', (req, res) => {
-//    res.send('Hello Express app!')
-res.render("login.ejs");
+  //    res.send('Hello Express app!')
+  res.render('login.ejs');
 });
-
 
 app.get('/login', (req, res) => {
-//    res.send('Hello Express app!')
-res.render("login.ejs");
+  res.render('login.ejs');
 });
+
+app.post('loginProcess', (req, res) => {
+  const { email, password } = req.body;
+  // HARD CODED
+
+})
 
 app.get('/home', (req, res) => {
-//    res.send('Hello Express app!')
-res.render("home.ejs");
+  //    res.send('Hello Express app!')
+  res.render('home.ejs');
 });
 
+app.get('/dbTest', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT CURDATE()');
+    res.send(rows);
+  } catch (err) {
+    console.error('Database error:', err);
+    res.status(500).send('Database error!');
+  }
+}); //dbTest
 
-
-
-app.get("/dbTest", async(req, res) => {
-   try {
-        const [rows] = await pool.query("SELECT CURDATE()");
-        res.send(rows);
-    } catch (err) {
-        console.error("Database error:", err);
-        res.status(500).send("Database error!");
-    }
-});//dbTest
-
-let port = 3000
-app.listen(port, ()=>{
-    console.log(`Website is live at port:${port}`);
-})
+let port = 3000;
+app.listen(port, () => {
+  console.log(`Website is live at port:${port}`);
+});
